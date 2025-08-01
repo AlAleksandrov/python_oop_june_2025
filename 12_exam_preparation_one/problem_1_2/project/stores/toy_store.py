9e8aa24cfcbf49680b47918bbd1416036d21ea82
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-08-01 12:01:03 +0300

    Create toy_store.py

---
+++ 12_exam_preparation_one/problem_1_2/project/stores/toy_store.py
@@ -0,0 +1,15 @@
+from project.stores.base_store import BaseStore
+
+
+class ToyStore(BaseStore):
+    CAPACITY = 100
+
+    def __init__(self, name: str, location: str):
+        super().__init__(name, location, self.CAPACITY)
+
+    @property
+    def store_type(self):
+        return "ToyStore"
+
+    def __str__(self):
+        return "Toys"
