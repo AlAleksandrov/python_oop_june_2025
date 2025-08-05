fded29b3afde747a844be5d4a0e713bbfe73f86a
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-08-05 22:00:29 +0300

    Create premium_influencer.py

---
+++ 15_self_exam_preparation/04_regular_exam_06_april_2024/problem_1_2/project/influencers/premium_influencer.py
@@ -0,0 +1,21 @@
+from project.campaigns.base_campaign import BaseCampaign
+from project.influencers.base_influencer import BaseInfluencer
+
+
+class PremiumInfluencer(BaseInfluencer):
+    INITIAL_PAYMENT = 0.85
+    CAMPAIGN_TYPE = {
+        "HighBudgetCampaign": 1.5,
+        "LowBudgetCampaign": 0.8
+    }
+
+    def __init__(self, username: str, followers: int, engagement_rate: float):
+        super().__init__(username, followers, engagement_rate)
+
+    def calculate_payment(self, campaign: "BaseCampaign"):
+        return self.INITIAL_PAYMENT * campaign.budget
+
+    def reached_followers(self, campaign_type: str):
+        if campaign_type in self.CAMPAIGN_TYPE:
+            return int(self.followers * self.engagement_rate * self.CAMPAIGN_TYPE[campaign_type])
+        return 0
